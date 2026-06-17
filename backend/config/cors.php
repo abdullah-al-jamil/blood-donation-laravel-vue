fb3617b5c656d7873993d0095c0dfb0d0700b215
Abdullah Al Jamil <jamilahmed284@gmail.com>
2026-06-17 12:47:51 +0600

    change origin

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,8 +4,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
